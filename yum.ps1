953765af33b5e062cef14af40695c5651da82f62
Gabriel <extofer@gmail.com>
2013-06-12 12:15:25 -0500

    Update yum.ps1
    added help and usage

--- yum.ps1
+++ yum.ps1
@@ -6,13 +6,28 @@
 $yumcmd= $args[0]
 $arg= $args[1]
 
+function yumHelp()
+{
+echo "Usage: yum COMMAND [argument]"
+echo " "
+echo "List of Commands:"
+echo "install"
+echo "search"
+echo "list"
+echo "remove"
+echo "help"
+
+}
+
 switch ($yumcmd)
 {
 	{($_ -eq "install") -or ($_ -eq "i")}{Install-Package $arg}
 	{($_ -eq "search") -or ($_ -eq "s")}{Get-Package -Filter $arg -ListAvailable}
 	{($_ -eq "list") -or ($_ -eq "1")}{if ($arg -eq "updates") {Get-Package -updates} else {Get-Package}}
 	{($_ -eq "remove") -or ($_ -eq "r")}{Uninstall-Package $arg}
-	default {"you must enter a valid argument. type yum -help for helpful usage message"} 
+	"help"{yumHelp}
+	default {yumHelp} 
 }
+
 
 #
